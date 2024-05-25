b7420229c9fe7bbdb83c6cbca4cf6c8df390680e
Adrian Steyer <adrian.steyer@posteo.de>
2024-05-25 15:27:42 +0200

    added music converter to mp3, better folder handling

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 from pytube import YouTube, Playlist
 import re
 import string
+from moviepy.editor import AudioFileClip
 
 def prompt_music():
     try:
@@ -18,33 +19,67 @@ def prompt_music():
 def download_video(link, music):
     yt = YouTube(link)
     print("Downloading: " + yt.title)
+
     if not music:
-        os.chdir(os.path.expanduser("~/Videos/YouTube"))
         yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first().download()
     else:
-        os.chdir(os.path.expanduser("~/Music/YouTube"))
-        yt.streams.filter(only_audio=True).order_by("abr").desc().first().download()
+        audio_file = yt.streams.filter(only_audio=True).order_by("abr").desc().first().download()
+        base, ext = os.path.splitext(audio_file)
+        new_file = base + '.mp3'
+        clip = AudioFileClip(audio_file)
+        clip.write_audiofile(new_file)
+        clip.close()
+        os.remove(audio_file)
+
 
 try:
     while True:
+        # obtain link from user
         try:
             link = input("Video/Playlist URL: \n")
         except KeyboardInterrupt:
             exit()
         
-        if not link.find("youtu") > 0:
+        if not "youtu" in link:
             print("Invalid link!")
             continue
-        
+        # prompt user for music
         music = prompt_music()
-        
+        # check if link is a playlist and download accordingly
         try:
-            if "playlist" in link:
+            if not "playlist" in link:
+                if not music:
+                    try:
+                        os.chdir(os.path.expanduser("~/Videos/YouTube"))
+                    except FileNotFoundError:
+                        os.mkdir(os.path.expanduser("~/Videos/YouTube"))
+                        os.chdir(os.path.expanduser("~/Videos/YouTube"))
+                else:
+                    try:
+                        os.chdir(os.path.expanduser("~/Music/YouTube"))
+                    except FileNotFoundError:
+                        os.mkdir(os.path.expanduser("~/Music/YouTube"))
+                        os.chdir(os.path.expanduser("~/Music/YouTube"))
+                
+                download_video(link, music)
+            else:
+                if not music:
+                    try:
+                        os.chdir(os.path.expanduser("~/Videos/YouTube/Playlists"))
+                    except FileNotFoundError:
+                        os.mkdir(os.path.expanduser("~/Videos/YouTube/Playlists"))
+                        os.chdir(os.path.expanduser("~/Videos/YouTube/Playlists"))
+                else:
+                    try:
+                        os.chdir(os.path.expanduser("~/Music/YouTube/Playlists"))
+                    except FileNotFoundError:
+                        os.mkdir(os.path.expanduser("~/Music/YouTube/Playlists"))
+                        os.chdir(os.path.expanduser("~/Music/YouTube/Playlists"))
+            
                 pl = Playlist(link)
                 pl._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
                 print('Number of videos in playlist: %s' % len(pl.video_urls))
                 print("Now downloading all videos from: " + pl.title)
-                os.chdir("Playlists")
                 a = pl.title
                 for c in list(string.punctuation):
                     a = a.replace(c, " ")
@@ -54,10 +89,10 @@ try:
                 os.chdir(a)
                 for v in pl.video_urls:
                     download_video(v, music)
-            else:
-                download_video(link, music)
+
         except Exception as e:
             print("Failed with an exception: " + str(e))
-            print("Done!")
+        print("Done! File(s) at: " + os.getcwd())
+
 except Exception as e:
     print("Terminated: " + e)
